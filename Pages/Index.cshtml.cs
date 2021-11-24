a15f8b45691d81940ec14c5ef29ea6ddc3927ae6
elip140 <epcotrim@gmail.com>
2021-11-24 14:58:56 -0300

    Webapp DOTNET Criado
    Webapp DOTNET Criado,
    Versão antiga está na Pasta "Tirar-Foto",
    Usando RazorPages.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tirar_Foto_DOTNET.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
