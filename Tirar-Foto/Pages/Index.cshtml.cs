5558a4b7c97a65033efb30c99c767dd0f73dae59
elip140 <epcotrim@gmail.com>
2021-12-08 17:16:04 -0300

    Recriação Do app

---
+++ Tirar-Foto/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TirarFoto.FotosService;
+using TirarFoto.Models;
+
+namespace Tirar_Foto.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    [BindProperty]
+    public Foto NewFoto { get; set; } = new();
+
+    public List<Foto> Lista = FotosList.GetAll();
+
+    public void OnGet()
+    {
+
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        FotosList.Add(NewFoto);
+        return RedirectToAction("Get");
+    }
+}
